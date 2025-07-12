3ab0913e2c96b767fa54c3f89ad2fbb216191ac6
balaji <117161525+balaji2005239@users.noreply.github.com>
2025-07-13 00:16:54 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
